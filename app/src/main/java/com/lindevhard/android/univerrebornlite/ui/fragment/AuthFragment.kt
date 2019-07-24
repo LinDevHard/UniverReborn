59a4f13ff96959123f8e275ade8d9c5bca972d00
LinDevHard <lindevhard@gmail.com>
2019-07-24 23:52:16 +0600

    Binding of view component

--- app/src/main/java/com/lindevhard/android/univerrebornlite/ui/fragment/AuthFragment.kt
+++ app/src/main/java/com/lindevhard/android/univerrebornlite/ui/fragment/AuthFragment.kt
@@ -2,15 +2,18 @@ package com.lindevhard.android.univerrebornlite.ui.fragment
 
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.lindevhard.android.univerrebornlite.R
 import com.lindevhard.android.univerrebornlite.utils.viewModelProvider
 import com.lindevhard.android.univerrebornlite.viewmodel.AuthViewModel
 import dagger.android.support.DaggerFragment
+import kotlinx.android.synthetic.main.fragment_auth.*
 import javax.inject.Inject
 
 /**
@@ -31,5 +34,16 @@ class AuthFragment : DaggerFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = viewModelProvider(viewModelFactory)
+
+        login_btn.setOnClickListener {
+            viewModel.authRequests(input_login.text.toString(), input_password.text.toString())
+        }
+
+        viewModel.login.observe(this, Observer { isLogin ->
+            if (isLogin) Log.d("AuthFragment", "Good Work") else
+                Log.d("AuthFragment", "Something don't work")
+        })
+
     }
 }
+
--- app/src/main/java/com/lindevhard/android/univerrebornlite/viewmodel/AuthViewModel.kt
+++ app/src/main/java/com/lindevhard/android/univerrebornlite/viewmodel/AuthViewModel.kt
@@ -1,6 +1,8 @@
 package com.lindevhard.android.univerrebornlite.viewmodel
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.lindevhard.android.univerrebornlite.api.AuthDataServer
@@ -8,23 +10,41 @@ import com.lindevhard.android.univerrebornlite.repository.AuthRepository
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class AuthViewModel @Inject constructor(val repository: AuthRepository) : ViewModel() {
+class AuthViewModel @Inject constructor(private val repository: AuthRepository) : ViewModel() {
+    private val TAG = "AuthViewModel"
+    private var isProcess = MutableLiveData<Boolean>()
+    private var isLogin = MutableLiveData<Boolean>()
+
+    val progress: LiveData<Boolean> = isProcess
+    val login: LiveData<Boolean> = isLogin
 
     init {
-        val data = AuthDataServer("test", "test")
+        //TODO: Create network checker
+        isProcess.value = false
+        isLogin.value = false
+    }
+
+    fun authRequests(login: String, password: String) {
+        if (!isProcess.value!!) {
+            Log.d("AuthViewModel", "Send Request")
+            sendAuthRequest(login, password)
+        }
+    }
+
+    private fun sendAuthRequest(login: String, password: String) {
         viewModelScope.launch {
-            try {
-                val response = repository.getAuthData(data = data)
+            runCatching {
+                isProcess.value = true
+                repository.getAuthData(AuthDataServer(login, password))
 
-                if (response.code != 0) {
-                    Log.d("AuthViewModel", response.message)
-                } else {
-                    Log.d("AuthViewModel", response.message)
-                }
-            } catch (e: Exception) {
-                Log.d("AuthViewModel", e.toString())
+            }.onSuccess {
+                Log.d(TAG, "onSuccess")
+                isLogin.value = true
+            }.onFailure {
+                Log.d(TAG, "onFailure")
+                isLogin.value = false
             }
-
+            isProcess.value = false
         }
     }
 }
