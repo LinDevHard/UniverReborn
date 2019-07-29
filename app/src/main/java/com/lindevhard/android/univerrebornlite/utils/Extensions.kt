fc8a8b3f55b431bae6746a453780ea684941a525
LinDevHard <lindevhard@gmail.com>
2019-07-29 22:11:19 +0600

    Created new ext func

--- app/src/main/java/com/lindevhard/android/univerrebornlite/utils/Extensions.kt
+++ app/src/main/java/com/lindevhard/android/univerrebornlite/utils/Extensions.kt
@@ -1,5 +1,7 @@
 package com.lindevhard.android.univerrebornlite.utils
 
+import android.widget.Toast
+import androidx.annotation.StringRes
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModel
@@ -25,3 +27,20 @@ inline fun <reified VM : ViewModel> FragmentActivity.viewModelProvider(
 inline fun <reified VM : ViewModel> Fragment.viewModelProvider(
         provider: ViewModelProvider.Factory
 ) = ViewModelProviders.of(this, provider).get(VM::class.java)
+
+/**
+ * For Fragments, show toast error messages
+ *  ```
+ *  showError(R.string.error_message)
+ *  ```
+ */
+fun Fragment.showError(@StringRes stringId: Int) {
+    Toast.makeText(this.context, stringId, Toast.LENGTH_LONG).show()
+}
+
+fun String.split(): String {
+    val index = this.indexOf(":")
+    if (index == -1)
+        return this
+    return this.substring(index + 1)
+}
