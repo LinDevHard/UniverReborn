989c78d9cf233aaa8432599bd4b10fb0525008a5
LinDevHard <lindevhard@gmail.com>
2019-08-01 23:33:25 +0600

    News fragment new layout created

--- app/src/main/java/com/lindevhard/android/univerrebornlite/ui/fragment/NewsFragment.kt
+++ app/src/main/java/com/lindevhard/android/univerrebornlite/ui/fragment/NewsFragment.kt
@@ -4,11 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.lindevhard.android.univerrebornlite.R
+import com.lindevhard.android.univerrebornlite.ui.adapter.NewsAdapter
 import com.lindevhard.android.univerrebornlite.utils.viewModelProvider
 import com.lindevhard.android.univerrebornlite.viewmodel.NewsViewModel
 import dagger.android.support.DaggerFragment
+import kotlinx.android.synthetic.main.fragment_exam_schedule.*
+import kotlinx.android.synthetic.main.fragment_profile.*
 import javax.inject.Inject
 
 class NewsFragment : DaggerFragment() {
@@ -26,6 +31,14 @@ class NewsFragment : DaggerFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = viewModelProvider(viewModelFactory)
+        recycleView.layoutManager = LinearLayoutManager(this.context)
+        viewModel.news.observe(this, Observer { item ->
+            swipe_refresher.isRefreshing = false
+            recycleView.adapter = NewsAdapter(item.news)
+        })
 
+        swipe_refresher.setOnRefreshListener {
+            viewModel.getNews()
+        }
     }
 }
--- app/src/main/java/com/lindevhard/android/univerrebornlite/viewmodel/NewsViewModel.kt
+++ app/src/main/java/com/lindevhard/android/univerrebornlite/viewmodel/NewsViewModel.kt
@@ -1,22 +1,29 @@
 package com.lindevhard.android.univerrebornlite.viewmodel
 
-import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.lindevhard.android.univerrebornlite.api.NewsList
 import com.lindevhard.android.univerrebornlite.repository.NewsRepository
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class NewsViewModel @Inject constructor(private val repo: NewsRepository) : ViewModel() {
+    private var newsData = MutableLiveData<NewsList>()
+    val news: LiveData<NewsList> = newsData
 
     init {
         getNews()
     }
 
-    private fun getNews() {
+    fun getNews() {
         viewModelScope.launch {
-            val data = repo.getNews()
-            Log.d("NEWS VIEW MODEL", data.toString())
+            runCatching {
+                repo.getNews()
+            }.onSuccess {
+                newsData.value = it.data
+            }
         }
     }
 
