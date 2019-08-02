2091d8a6ea24cfbed00ca03a95b22c2e46a2b82b
LinDevHard <lindevhard@gmail.com>
2019-08-02 23:16:01 +0600

    Attendance deserializer implemented

---
+++ app/src/main/java/com/lindevhard/android/univerrebornlite/utils/AttendanceDeserializer.kt
@@ -0,0 +1,33 @@
+package com.lindevhard.android.univerrebornlite.utils
+
+import android.util.Log
+import com.google.gson.JsonDeserializationContext
+import com.google.gson.JsonDeserializer
+import com.google.gson.JsonElement
+import com.lindevhard.android.univerrebornlite.api.Attendance
+import com.lindevhard.android.univerrebornlite.api.AttendanceList
+import java.lang.reflect.Type
+
+class AttendanceDeserializer : JsonDeserializer<AttendanceList> {
+    override fun deserialize(json: JsonElement, typeOfT: Type, context: JsonDeserializationContext): AttendanceList {
+        Log.d("AttendanceDeserializer", json.toString())
+        val rootJson = json.asJsonArray
+        val data = mutableListOf<Attendance>()
+        for (element in rootJson) {
+            Log.d("AttendanceDeserializer", element.toString())
+            if (element.isJsonArray) {
+                val subjectRoot = element.asJsonArray
+                val attendData = Attendance()
+                attendData.subjectName = subjectRoot[0].asJsonObject.get("name_1").asString
+                attendData.type = subjectRoot[0].asJsonObject.get("name_2").asString
+                attendData.period = subjectRoot[0].asJsonObject.get("name_3").asString
+                attendData.groupId = subjectRoot[0].asJsonObject.get("groupInfo").asString
+
+                attendData.firstRk = subjectRoot[1].asJsonObject.get("sum").asInt
+                attendData.secondRk = subjectRoot[2].asJsonObject.get("sum").asInt
+                data += attendData
+            }
+        }
+        return AttendanceList(data)
+    }
+}
