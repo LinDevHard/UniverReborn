9dc992cf5bad3cd3a39220a9f9892a82ac4dc280
LinDevHard <lindevhard@gmail.com>
2019-08-05 23:30:21 +0600

    new field added(for test)

--- app/src/main/java/com/lindevhard/android/univerrebornlite/api/AttendanceApi.kt
+++ app/src/main/java/com/lindevhard/android/univerrebornlite/api/AttendanceApi.kt
@@ -1,8 +1,12 @@
 package com.lindevhard.android.univerrebornlite.api
 
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
 
 interface AttendanceApi {
+    @FormUrlEncoded
     @POST("/student/attendance")
-    suspend fun getAttendance(): ApiResponse<AttendanceList>
+    suspend fun getAttendance(@Field("semester") semester: Int,
+                              @Field("year") year: Int): ApiResponse<AttendanceList>
 }
